8f11ac39c47b5a1a544638c4709ffcf317062d8c
Rajeev Ranjan <ranjan@Rajeevs-MacBook-Air-3.local>
2024-11-11 01:10:24 +0530

    Loader Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,29 @@
-import './App.css'
+import './App.css';
+import Home from '../src/pages/Home';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 2000);
+  }, []);
 
   return (
     <>
-      <div>
-
-      </div>
+      {loading ? (
+        <div className="loader-container">
+          <div className="bounce-loader">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      ) : (
+        <Home />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../components/navbar/Navbar'
+import Navbar from '../components/navbar/Navbar'
+
+const Home = () => {
+  return (
+    <>
+    <div className='m-0 p-0 bg-black h-screen w-screen'>
+
+    <Navbar/>
+    </div>
+    </>
+  )
+}
+
+export default Home
